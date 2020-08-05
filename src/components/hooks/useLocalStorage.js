85e88e66a7e03b388d7c57d9115891fda4745db6
MMendes24 <mmbusiness1@protonmail.com>
2020-08-05 15:36:45 -0400

    First hook built.

---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export const useLocalStorage = (key, initialValue) => {
+    // initital state is a function that checks to see if the initial
+    //argument already exists in local storage, else returning it to local storage
+    const [ storedValue, setStoredValue] = useState( () => {
+        //get from local storage by key
+        const item = window.localStorage.getItem(key)
+        // parse and return stored json, or if undefined, return initialValue
+        return item ? JSON.parse(item) : initialValue
+    })
+    return [ storedValue ]
+}
+
